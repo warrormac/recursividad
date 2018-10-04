2c3ec908459a8d06d602295fc2514d2899396a2c
andre <warrormac@gmail.com>
2018-10-03 19:22:26 -0500

    ordenamiento

---
+++ ordenamiento con punteros.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int print(int *ptr, int tam)
+{
+    if (tam==0)
+    {
+        return 1;
+    }
+    cout<<*ptr;
+    print(ptr+1,tam-1);
+
+}
+
+int ordenamiento(int *ptr, int tam)
+{
+    int b=1,c=0;
+    while (c++<tam)
+    {
+        while (b++<tam)
+        {
+            if (*(ptr+c)>*(ptr+b))
+            {
+                int temp=*(ptr+c);
+                *(ptr+c)=*(ptr+b);
+                *(ptr+b)=temp;
+            }
+
+        }
+        b=c+1;
+    }
+}
+
+
+
+int main()
+{
+    int x[]={1,5,2,8,6,3};
+    int *ptr=x;
+    ordenamiento(ptr,6);
+    print(ptr,6);
+}
